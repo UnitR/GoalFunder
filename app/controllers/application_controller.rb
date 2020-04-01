83411ef13d165772a13a577b9303e9d5d09cdaf1
Angel Peltekov <ai.peltekov@gmail.com>
2020-04-01 20:11:22 +0100

    Error handling in base ApplicationController for unauthorized access.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  # Specify how to handle NotAuthorized
+  rescue_from User::NotAuthorized, with: :user_not_authorized
+
+  private
+
+  # The user is trying to do something illegal so stop them nicely.
+  def user_not_authorized
+    # Show the user a friendly error message and redirect
+    flash[:error] = "You don't have access to this section."
+    redirect_back(fallback_location: root_path)
+  end
+
 end
