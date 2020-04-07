d1f3e89a445070374ee9972ba8af85e074d7d474
harv <harvinder_bedi@hotmail.co.uk>
2020-04-07 17:36:02 +0100

    devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-
-  has_many :goals, through: :goal_owners
-  has_many :groups
-  has_many :user_group_memberships
-
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
