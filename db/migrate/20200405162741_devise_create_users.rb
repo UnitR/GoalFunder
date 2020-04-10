7c750a8af01292305ffd031cd11edcde31c42804
Angel Peltekov <ai.peltekov@gmail.com>
2020-04-10 17:59:15 +0100

    Migrate database to use Devise users.

--- db/migrate/20200405162741_devise_create_users.rb
+++ db/migrate/20200405162741_devise_create_users.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class DeviseCreateUsers < ActiveRecord::Migration[6.0]
+
   def change
-    create_table :users do |t|
+
+    change_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.change :email, :string, null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -15,11 +17,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.datetime :remember_created_at
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.inet     :current_sign_in_ip
-      # t.inet     :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.inet     :current_sign_in_ip
+      t.inet     :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token
@@ -32,13 +34,16 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
-      t.timestamps null: false
+      # Don't add timestamps (already present)
+      # t.timestamps null: false
+      #
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
+
   end
+
 end
