25a2ad9983babdad7c742bd6b1e0473a61b508a1
Angel Peltekov <ai.peltekov@gmail.com>
2020-04-10 18:12:24 +0100

    Preventing user authentication requests for controller actions. This is temporary while still working on overall functionality and design.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   # Specify how to handle NotAuthorized
   # rescue_from User::NotAuthorized, with: :user_not_authorized
