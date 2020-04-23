e2acb5b8f03a4c55615386e58fd58bdef082e798
alexhernandez00 <ah01379@surrey.ac.uk>
2020-04-23 11:35:34 +0200

    added sidebar and javascript to make it work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 import "bootstrap"
-import "../stylesheets/application"
+
 
 
 require("@rails/ujs").start()
@@ -14,9 +14,21 @@ require("channels")
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+//= require tree .
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+document.addEventListener("turbolinks:load", function() {
+  // Here goes your jQuery ready code
+  $('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+      console.log("MouseLeave!");
+  });
+
+});
