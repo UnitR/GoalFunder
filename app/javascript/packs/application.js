eeb35309bba3f7417447a69a22b20cbc6616f6d7
Angel Peltekov <ai.peltekov@gmail.com>
2020-04-25 18:54:20 +0100

    Rebuild configuration to hopefully avoid some issues. Changed jQuery inclusion to fix its import problems.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,14 +2,12 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import "bootstrap"
 
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 
-require('jquery');
 require('popper.js');
 require('bootstrap');
 //= require tree .
