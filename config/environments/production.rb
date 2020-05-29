4bfe5e309471ea639e1d76cfc2cb0f560f1a00fd
Angel Peltekov <ai.peltekov@gmail.com>
2020-05-29 23:09:07 +0100

    Fixes for ActionCable for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,6 +110,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   #
-  cconfig.web_socket_server_url = "wss://goal-funder.herokuapp.com/cable"
+  config.web_socket_server_url = "wss://goal-funder.herokuapp.com/cable"
   config.action_cable.allowed_request_origins = ['https://goal-funder.herokuapp.com', 'http://goal-funder.herokuapp.com']
 end
